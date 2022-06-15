fe314c749e4dc3387ba799488022b2c10efce19d
Suroo <SDHUGHES1998@GMAIL.COM>
2022-06-15 19:18:36 +0000

    updated edit body form_classes

--- myblog/forms.py
+++ myblog/forms.py
@@ -12,3 +12,14 @@ class PostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Post Here...'}),
         }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title Here...'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Post Here...'}),
+        }
