481a8b0e9e0478810d7756df11a1d6b852fa0296
Suroo <SDHUGHES1998@GMAIL.COM>
2022-06-26 01:52:40 +0000

    added js

--- myblog/views.py
+++ myblog/views.py
@@ -21,6 +21,12 @@ class HomeView(ListView):
     #ordering = ['-id']
     ordering = ['blog_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
